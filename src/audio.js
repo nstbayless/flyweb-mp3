0ff9012b1b639f9fc832282e5cd0cb2d7566f174
sino <sinoforever@outlook.com>
2016-10-18 10:34:17 -0400

    chmod www

--- src/audio.js
+++ src/audio.js
@@ -1,134 +1,3 @@
-<<<<<<< HEAD
-// pops queue, plays audio
-
-repeat = require('repeat');
-
-tmp = require('./tmp_store');
-Song = require('./_song');
-manager = require('./playlist_manager');
-
-// audio playing pipeline:
-var Player = require('player');
-var Speaker = require('speaker');
-var Lame = require('lame');
-var Fs = require('fs');
-var Throttle = require('throttle');
-var Stream = require('stream');
-
-var current_player = new Player();
-var current_timer = 0; // timer records time elapsed
-var current_state = "paused"; // state of the music
-var current_song_duration = 0; // the length of the current song
-
-var speaker;
-var prevFlag = false;
-
-
-// plays song with audio pipeline above
-function play_file(file, cb) {
-    console.log(file);
-
-    var stream = Fs.createReadStream(file);
-    var decoder = new Lame.Decoder();
-    var totalBytesSeen = 0;
-    var rate = 0;
-
-    var transform = new Stream.Transform({
-        transform: function (chunk, encoding, callback) {
-            this.push(chunk);
-            totalBytesSeen += chunk.length;
-            current_timer = totalBytesSeen / (4 * rate);
-            callback();
-        }
-    });
-
-    decoder.on("format", (format) => {
-        console.log("gaagaghahahha");
-        console.log('MP3 format: %j', format);
-        speaker.channels = format.channels;
-        speaker.bitDepth = format.bitDepth;
-        speaker.sampleRate = format.sampleRate;
-        rate = format.sampleRate;
-    })
-
-    speaker = new Speaker({
-        channels: 2,
-        bitDepth: 16,
-        sampleRate: 10
-    });
-
-    speaker.on('pipe', () => {
-        console.log("***************************************");
-        current_state = "playing";
-    });
-
-    //stream = stream.pipe(new Throttle(44100));
-    stream.pipe(decoder).pipe(transform).pipe(speaker);
-
-    speaker.on('finish', () => {
-        current_timer = 0;
-        current_state = "paused";
-
-        if (!prevFlag) {
-            manager.nextSong((err, s) => {
-                play(s)
-            });
-        } else {
-            manager.prevSong((err, s) => {
-                play(s)
-            });
-        }
-
-        prevFlag = false;
-    });
-}
-
-function prev() {
-    prevFlag = true;
-    speaker.end();
-    return current_state;
-}
-
-function next() {
-    prevFlag = false;
-    speaker.end();
-    return current_state;
-}
-
-// pauses the song
-function pause() {
-    if (current_state === 'paused') {
-        current_state = 'playing';
-        speaker.uncork();
-    } else {
-        if (current_state === 'playing') {
-            current_state = 'paused';
-	          speaker.cork();
-        }
-    }
-
-    return current_state;
-}
-
-// takes song metadata, makes playable information for update below
-function play(song) {
-    // copy song metadata into realized object:
-    var re = {props: song};
-    // adjusts re object based on song type:
-    if (song.type == "silence" || song.type == "empty") {
-        re.props.name = "Nothing Playing";
-        re.t_elapsed = 0;
-    }
-    else if (song.type == "upload") {
-        re.t_elapsed = 0;
-        console.log("Now playing: " + song.name);
-        current_song_duration = song.duration;
-        re.state = "play";
-        play_file(song.path, (ev) => {
-            //handle event:
-            if (ev == "finish") {
-                tmp.track.state = "finish";
-
 module.exports = function(io) {
 	// pops queue, plays audio
 	repeat = require('repeat');
@@ -163,37 +32,49 @@ module.exports = function(io) {
 	}
 
 	//plays song with audio pipeline above
-	function play_file(file, cb) {
+    function play_file(file, cb) {
+        console.log(file);
 
-		var stream = Fs.createReadStream(file);
-		var totalBytesSeen = 0;
-		var transform = new Stream.Transform({
-			transform: function(chunk, encoding, callback) {
-				this.push(chunk);
-				totalBytesSeen += chunk.length;
-				current_timer = totalBytesSeen / (4 * 44100);
-				emitStatus(tmp.track.props.name, current_state, current_song_duration, current_timer);
-				callback();
-			}
-		});
+        var stream = Fs.createReadStream(file);
+        var decoder = new Lame.Decoder();
+        var totalBytesSeen = 0;
+        var rate = 0;
 
-		speaker = new Speaker({
-			channels: 2,
-			bitDepth: 16,
-			sampleRate: 44100
-		});
+        var transform = new Stream.Transform({
+            transform: function (chunk, encoding, callback) {
+                this.push(chunk);
+                totalBytesSeen += chunk.length;
+                current_timer = totalBytesSeen / (4 * rate);
+                callback();
+            }
+        });
 
-		speaker.on('pipe', () => {
-			console.log("***************************************");
-			current_state = "playing";
-		});
+        decoder.on("format", (format) => {
+            console.log("gaagaghahahha");
+            console.log('MP3 format: %j', format);
+            speaker.channels = format.channels;
+            speaker.bitDepth = format.bitDepth;
+            speaker.sampleRate = format.sampleRate;
+            rate = format.sampleRate;
+        })
 
-		//stream = stream.pipe(new Throttle(44100));
-		stream.pipe(Lame.Decoder()).pipe(transform).pipe(speaker);
+        speaker = new Speaker({
+            channels: 2,
+            bitDepth: 16,
+            sampleRate: 10
+        });
 
-		speaker.on('finish', () => {
-			current_timer = 0;
-			current_state = "paused";
+        speaker.on('pipe', () => {
+            console.log("***************************************");
+            current_state = "playing";
+        });
+
+        //stream = stream.pipe(new Throttle(44100));
+        stream.pipe(decoder).pipe(transform).pipe(speaker);
+
+        speaker.on('finish', () => {
+            current_timer = 0;
+            current_state = "paused";
 
             if (!prevFlag) {
                 manager.nextSong((err, s) => {
@@ -205,9 +86,9 @@ module.exports = function(io) {
                 });
             }
 
-			prevFlag = false;
-		});
-	}
+            prevFlag = false;
+        });
+    }
 
     function prev() {
 		prevFlag = true;
