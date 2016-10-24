464694ccc595550c53a5553b6f5b9816060db9c0
N. Bayless <nstbayless@gmail.com>
2016-10-24 15:23:03 -0700

    Linted src/audio_manager.js

--- src/audio_manager.js
+++ src/audio_manager.js
@@ -1,6 +1,3 @@
-var Playlist = require('./_playlist');
-var Song = require('./_song');
-
 var audio_manager = {};
 
 //the song currently playing
@@ -11,42 +8,42 @@ audio_manager.prev_flag = false;
 
 audio_manager.current_is_empty = function() {
     return !audio_manager.current_song || audio_manager.current_song.type == 'empty';
-}
+};
 
 audio_manager.is_playing = function() {
     return audio_manager.play_state == 'playing';
-}
+};
 
 audio_manager.is_paused = function() {
     return audio_manager.play_state == 'paused';
-}
+};
 
 audio_manager.get_state = function() {
     return audio_manager.play_state;
-}
+};
 
 audio_manager.get_title = function() {
     return audio_manager.current_song.name;
-}
+};
 
 audio_manager.get_duration = function() {
     return audio_manager.current_song.duration;
-}
+};
 
 audio_manager.get_time_elapsed = function() {
     return audio_manager.time_elapsed;
-}
+};
 
 audio_manager.set_current = function(song) {
     audio_manager.current_song = song;
-}
+};
 
 audio_manager.set_time_elapsed = function(time) {
     audio_manager.time_elapsed = time;
-}
+};
 
 audio_manager.set_state = function(state) {
     audio_manager.play_state = state;
-}
+};
 
 module.exports = audio_manager;
